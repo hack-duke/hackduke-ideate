c91e85fa31cb3d56ba9163702200138c12f11d04
edl13 <edl13@duke.edu>
2016-04-05 01:43:14 -0400

    scroll bug

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
 $(window).scroll(function (event) {
     var scroll = $(window).scrollTop();
     if(scroll>5){
-		$('#custom-bootstrap-menu').addClass('menuScrolled');
-		$('#custom-bootstrap-menu.navbar-default .navbar-nav>li>a').addClass('linkScrolled');
+		//$('#custom-bootstrap-menu').addClass('menuScrolled');
+		//$('#custom-bootstrap-menu.navbar-default .navbar-nav>li>a').addClass('linkScrolled');
 		//$('.navlogoblack').removeClass('logoScrolled');
 		//$('.navlogowhite').addClass('logoScrolled');
 	}else{
-		$('#custom-bootstrap-menu').removeClass('menuScrolled');
-		$('#custom-bootstrap-menu.navbar-default .navbar-nav>li>a').removeClass('linkScrolled');
+		//$('#custom-bootstrap-menu').removeClass('menuScrolled');
+		//$('#custom-bootstrap-menu.navbar-default .navbar-nav>li>a').removeClass('linkScrolled');
 		//$('.navlogoblack').addClass('logoScrolled');
 		//$('.navlogowhite').removeClass('logoScrolled');	
 	}
