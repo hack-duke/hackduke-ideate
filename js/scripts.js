ea5b05ec851104a43980e0a7b4b198998c0d4da7
edl13 <edl13@duke.edu>
2016-04-05 01:33:46 -0400

    changed EERERYTHINGLOL

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,19 @@ $(window).scroll(function (event) {
     if(scroll>5){
 		$('#custom-bootstrap-menu').addClass('menuScrolled');
 		$('#custom-bootstrap-menu.navbar-default .navbar-nav>li>a').addClass('linkScrolled');
-		$('.navlogoblack').removeClass('logoScrolled');
-		$('.navlogowhite').addClass('logoScrolled');
+		//$('.navlogoblack').removeClass('logoScrolled');
+		//$('.navlogowhite').addClass('logoScrolled');
 	}else{
 		$('#custom-bootstrap-menu').removeClass('menuScrolled');
 		$('#custom-bootstrap-menu.navbar-default .navbar-nav>li>a').removeClass('linkScrolled');
-		$('.navlogoblack').addClass('logoScrolled');
-		$('.navlogowhite').removeClass('logoScrolled');	
+		//$('.navlogoblack').addClass('logoScrolled');
+		//$('.navlogowhite').removeClass('logoScrolled');	
+	}
+	
+	if(scroll>250){
+		$('.navlogo').removeClass('logo-transparent');
+	}else{
+		$('.navlogo').addClass('logo-transparent');
 	}
 	
 });
